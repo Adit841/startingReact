1f282bb9056fe288ec1015d62dbde1060fdec8e8
Adit841 <adityagour0841@gmail.com>
2026-03-20 20:54:41 +0530

    working

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Profile from './components/Profile'
+import Login from './components/Login'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
  
   return (
-    <>
+    <UserContextProvider>
     <h1>Hello boiya</h1>
-     </>
+    <Login />
+    <Profile />
+     </UserContextProvider>
   )
 }
 
--- miniContext/src/context/UserContextProvider.jsx
+++ miniContext/src/context/UserContextProvider.jsx
@@ -5,7 +5,7 @@ const UserContextProvider = ({children}) => {
     const [user, setUser] = React.useState(null);
 
     return(
-        <UserContext.Provider value={{user}}>
+        <UserContext.Provider value={{user, setUser}}>
             {children}        
         </UserContext.Provider>
     )
